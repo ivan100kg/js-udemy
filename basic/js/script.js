82149aa1835849ebb57f104a569676abd6d82ef1
IvanB <ivar110kg@gmail.com>
2022-12-28 21:14:21 +0400

    Add new lesson

--- basic/js/script.js
+++ basic/js/script.js
@@ -2,3 +2,24 @@
 
 let number = 5;
 const leftBorderWidth = 2;
+
+console.log(4/-0);
+console.log(4/'dui');
+
+const obj = {
+    name: 'Jon',
+    surname: 'Snow'
+};
+
+console.log(obj);
+
+let x=1, y=1;
+console.log(1+ x++);
+console.log(1+ ++y);
+
+console.log(true&&true);
+console.log(true&&false);
+console.log(false&&false);
+console.log(true||true);
+console.log(true||false);
+console.log(false||false);
