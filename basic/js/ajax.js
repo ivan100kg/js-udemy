6d7aa1e67ec984f33eb24f74a55ddeb2b111f763
IvanB <ivar110kg@gmail.com>
2023-03-18 13:44:37 +0400

    Add POST

--- basic/js/ajax.js
+++ basic/js/ajax.js
@@ -1,5 +1,11 @@
 'use strict';
 
+const message = {
+    loading: 'Загрузка',
+    success: 'Успешно',
+    failure: 'Что-то пошло не так'
+};
+
 const inputField = document.querySelector('#usd');
 const outputField = document.querySelector('#rub');
 inputField.addEventListener('input', () => {
@@ -18,5 +24,39 @@ inputField.addEventListener('input', () => {
             outputField.value = 'unknown';
         }
     });
-
 });
+
+function postData(form) {
+    form.addEventListener('submit', (e) => {
+        e.preventDefault();
+
+        const status = document.createElement('div');
+        status.classList.add('status');
+        status.textContent = message.loading;
+
+        const request = new XMLHttpRequest();
+        request.open('POST', 'server/');            // POST
+
+        const formData = new FormData(form);        // спец объект формы(устар)
+
+        // FormData to JSON
+        request.setRequestHeader('Content-type', 'application/json');
+        const obj = {};
+        formData.forEach((value, key) => obj[key] = value);
+        const json = JSON.stringify(obj);
+
+        // request.send(formData);                     // отправить FormData
+        request.send(json);                         // отправить JSON
+        request.addEventListener('load', () => {
+            if(request.status === 200) {
+                console.log(request.response);
+                status.textContent = message.success;
+                form.reset();
+                setTimeout(() => status.remove(), 2000);
+            } else {
+                console.log(request.response);
+                status.textContent = message.failure;
+            }
+        });
+    });
+}
