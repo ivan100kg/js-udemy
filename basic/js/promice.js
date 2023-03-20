7c4e20ceba74d71ff1b5dcfc11ee40f6cab0b7c0
IvanB <ivar110kg@gmail.com>
2023-03-20 15:02:07 +0400

    Update promise

--- basic/js/promice.js
+++ basic/js/promice.js
@@ -17,36 +17,37 @@ console.log('Request data...');
 //     }, 2000);
 // }, 2000);
 
-const promise = new Promise((resolve, reject) => {
-    setTimeout(() => {
-        console.log('Preparing data...');
+// const promise = new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//         console.log('Preparing data...');
     
-        const backendData = {
-            server: 'aws',
-            port: 2000,
-            status: 'ok'
-        };
-        resolve(backendData);
-    }, 2000);
-});
+//         const backendData = {
+//             server: 'aws',
+//             port: 2000,
+//             status: 'ok'
+//         };
+//         resolve(backendData);
+//     }, 2000);
+// });
 
-promise.then(backendData => {
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            backendData.moidified = true;
-            reject(backendData);
-        }, 2000);
-    })
-        .then(clientData => {
-            clientData.hui = true;
-            return clientData;
-        })
-        .then(data => console.log('modified', data))
-        .catch(err => console.log('Error', err))
-        .finally(() => console.log('finally'));
-});
+// promise.then(backendData => {
+//     return new Promise((resolve, reject) => {
+//         setTimeout(() => {
+//             backendData.moidified = true;
+//             reject(backendData);
+//         }, 2000);
+//     })
+//         .then(clientData => {
+//             clientData.hui = true;
+//             return clientData;
+//         })
+//         .then(data => console.log('modified', data))
+//         .catch(err => console.log('Error', err))
+//         .finally(() => console.log('finally'));
+// });
 
-// const test = time => new Promise(resolve => setTimeout(() => resolve(), time));
+const test = time => new Promise(resolve => setTimeout(() => resolve(), time));
 
-// Promise.all([test(1000), test(2000)]).then(() => console.log('success'));
+Promise.all([test(1000), test(2000)]).then(() => console.log('success'));
+Promise.race([test(1000), test(2000)]).then(() => console.log('success'));
 
